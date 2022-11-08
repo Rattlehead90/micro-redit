08b8d9c9adf081543f8cf0e6599be51ea93b3d76
Rattlehead90 <strogonov@seznam.cz>
2022-11-08 22:29:22 +0100

    Add fixtures to tests to handle associations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,13 +2,7 @@ require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
   def setup
-    fake_content = <<-LOREM
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum
-      enim vitae libero laoreet, quis rhoncus nisi tempor. Ut convallis leo vitae
-      sapien dignissim efficitur. Nam eget mi diam. Pellentesque nec egestas justo,
-      sed egestas tellus. Phasellus porttitor eget tellus.
-    LOREM
-    @post = Post.new(title: 'Example Title', content: fake_content )
+    @post = posts(:one)
   end
 
   test 'should have a title' do
@@ -34,7 +28,7 @@ class PostTest < ActiveSupport::TestCase
   end
 
   test 'content should not exceed 2960 characters' do
-    @post.content = @post.content * 10
+    @post.content = 'a' * 2961
     assert_not @post.valid?
   end
 end
